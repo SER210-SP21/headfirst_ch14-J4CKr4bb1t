10cda58a4942c7010e979a1d5b126e267a1221e6
sarahallerheiligen <sballerheiligen@quinnipiac.edu>
2024-04-25 19:18:20 -0400

    task runtimeException: cannot find TaskDatabase

---
+++ app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -0,0 +1,31 @@
+package com.hfad.tasks
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    //without suspend, they would NOT run in the background
+    //this can block other methods cuz databases can be slow sometimes
+    @Insert
+    suspend fun insert (task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    //these use live data, so they do not need to be suspended. they ALREADY run in the background
+    @Query("SELECT * FROM task_table WHERE taskId = :taskId")
+    fun get(taskId: Long): LiveData<Task>
+
+    @Query("SELECT * FROM task_table ORDER BY taskId DESC")
+    fun getAll(): LiveData<List<Task>>
+
+}
